2531286d6296d4d9b1d81f401c75c6df85ca382d
hariprasathvk-code <hariprasath.krishnamoorthi@psiog.com>
2025-09-27 23:56:00 +0530

    formatDoc fuctionality commit

---
+++ script.js
@@ -0,0 +1,13 @@
+    function formatdoc(cmd, value=null) {
+        if(value) {
+            document.execCommand(cmd, false, value);
+        } else {
+            document.execCommand(cmd);
+        }
+    }
+
+function addLink() {
+	const url = prompt('Insert url');
+	formatDoc('createLink', url);
+}
+
